1b5cfca644c32ee7380f8b9470ac207aa4a1356a
Zhijie He <hezhijie0327@hotmail.com>
2024-10-01 22:07:55 +0800

    🔨 chore: rollback changes

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -22,6 +22,12 @@ export default async (req: Request) => {
   try {
     const { q, language, time_range, safesearch, categories, engines} = (await req.json()) as SearchParameters;
 
+    if (!q) {
+      return createErrorResponse(PluginErrorType.PluginRequestParameterInvalid, {
+        message: 'Search query (q) is required.',
+      });
+    }
+
     const searchParameters: SearchParameters = {
       q,
       format: 'json',
