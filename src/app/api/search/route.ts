28c5fe554a6236f6d9d0c84d4be30c094b1a470f
Zhijie He <hezhijie0327@hotmail.com>
2024-10-02 18:51:45 +0800

    Update route.ts

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -4,7 +4,7 @@ import {
   createErrorResponse,
   getPluginSettingsFromRequest,
 } from '@lobehub/chat-plugin-sdk';
-import { SearchParameters, SearchResponse, Settings } from '@/type';
+import { SearchParameters, Settings } from '@/type';
 
 export async function POST(req: NextRequest) {
   const settings = getPluginSettingsFromRequest<Settings>(req);
@@ -53,7 +53,7 @@ export async function POST(req: NextRequest) {
       method: 'POST',
     });
 
-    let results = (await response.json()) as SearchResponse;
+    let results = await response.json();
     results.results = results.results.slice(0, max_results);
     console.log('Search Results:', results.results);
 
