fe776858ae0dcb1935503ea0dd3f4b2b5c15722a
Zhijie He <hezhijie0327@hotmail.com>
2024-10-02 18:51:10 +0800

    Update index.ts

--- src/type/index.ts
+++ src/type/index.ts
@@ -8,19 +8,6 @@ export interface SearchParameters {
   time_range?: string;
 }
 
-export interface SearchResult {
-  content: string;
-  engine: string;
-  score?: number;
-  title: string;
-  url: string;
-}
-
-export interface SearchResponse {
-  query: string;
-  results: SearchResult[];
-}
-
 export interface Settings {
   SEARXNG_INSTANCE_URL: string;
   SEARXNG_MAX_RESULTS?: number;
