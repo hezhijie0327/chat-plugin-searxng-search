d710a17b0c2ce6f38fcde4cf2d89f975a4a0268a
Zhijie He <hezhijie0327@hotmail.com>
2024-10-02 13:23:30 +0800

    🔨 chore: rollback changes

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -56,7 +56,7 @@ export async function POST(req: NextRequest) {
     let results = (await response.json()) as SearchResponse;
 
     // Limit the results to max_results
-    results.results = results.results.slice(0, max_results - 1);
+    results.results = results.results.slice(0, max_results);
 
     console.log('Search Results:', results.results);
 
