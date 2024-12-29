864b3bb51807d142fc7dcdf6b42ef1197b8b908a
Zhijie He <hezhijie0327@hotmail.com>
2024-12-29 17:14:33 +0800

    🔨 chore: cleanup code

--- src/app/api/gateway/route.ts
+++ src/app/api/gateway/route.ts
@@ -1,11 +1,4 @@
-import {NextRequest, NextResponse} from "next/server";
 import {createGatewayOnEdgeRuntime} from "@lobehub/chat-plugins-gateway";
-
-/*
-export async function OPTIONS(req: NextRequest, res: NextResponse) {
-  return createGatewayOnEdgeRuntime()(req)
-}
-*/
 
 export async function POST(req: Request) {
   console.log('SearXNG Search Plugin Gateway for LobeChat', process.env.NODE_ENV)
