9620e2405fbdcfb375ac34440487287293452ab3
Zhijie He <hezhijie0327@hotmail.com>
2024-12-30 12:41:53 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const buildWithDocker = process.env.DOCKER === 'true';
+
 const nextConfig = {
-  output: 'standalone',
+  compress: isProd,
+  output: buildWithDocker ? 'standalone' : undefined,
   async headers() {
     return [
       {
